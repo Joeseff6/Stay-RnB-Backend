fcda89428fe81c30118896f3d02deb18a1d5e01b
Joseph Soria <josephsoria6@gmail.com>
2021-05-21 21:12:46 -0500

    added line for serving client/build

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ const logger = require(`morgan`);
 const mongoose = require(`mongoose`);
 const PORT = process.env.PORT || 3001;
 const routes = require(`./controllers`);
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const path = require("path");
-
-
 const app = express();
-
-const session = require('express-session');
+const session = require("express-session");
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: MongoStore.create({
     mongoUrl: process.env.MONGODB_URI || `mongodb://localhost/Stay_RnB`,
-  })
+  }),
 };
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/Stay_RnB`, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 app.use(session(sess));
 app.use(logger(`dev`));
@@ -28,12 +34,7 @@ app.use(express.static(`public`));
 app.use(routes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/Stay_RnB`, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
